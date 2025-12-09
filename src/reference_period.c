be5c45290e351cb68d033134e9685976f0ddad10
David Frantz <david.frantz@uni-trier.de>
2025-12-09 15:43:28 +0100

    iteratively find stable reference period

--- src/reference_period.c
+++ src/reference_period.c
@@ -23,13 +23,14 @@
 #include "utils/stats.h"
 
 typedef struct {
+  int n_cpus;
   int n_images;
   char **path_input;
+  char path_mask[STRLEN];
   char path_output_reference_period[STRLEN];
   char path_output_coefficient[STRLEN];
   int modes;
   int trend;
-  int current;
   int fit_end;
   int threshold;
   int confirmation_number;
@@ -38,19 +39,24 @@ typedef struct {
 
 void usage(char *exe, int exit_code){
 
-  printf("Usage: %s -r output-reference-period-image -c output-coefficient-image input-image(s)\n", exe);
-  printf("          -m modes -t trend -e end-fitting -s threshold -n confirmation-number -y current-year\n");
+  printf("Usage: %s -j cpus -x mask-image -r output-reference-period-image -c output-coefficient-image\n", exe);
+  printf("          -m modes -t trend -e end-fitting -s threshold -n confirmation-number input-image(s)\n");
   printf("\n");
+  printf("  -j = number of CPUs to use\n");
+  printf("\n");
+  printf("  -x = mask image\n");
   printf("  -r = output reference period image (e.g., reference_period.tif)\n");
   printf("  -c = output coefficient image (e.g., coefficient.tif)\n");
+  printf("\n");
   printf("  -m = number of modes for fitting the harmonic model (1-3)\n");
   printf("  -t = use trend coefficient when fitting the harmonic model? (0 = no, 1 = yes)\n");
   printf("  -e = end year for initial fitting (e.g., 2017)\n");
   printf("  -s = threshold for detecting change (e.g., 500)\n");
   printf("  -n = confirmation number for detecting change (e.g., 3)\n");
-  printf("  -y = current year (e.g., 2025)\n");
   printf("\n");
   printf("  input-image(s) = input images to compute reference period from\n");
+  printf("                   images must be ordered by date (earliest to latest)\n");
+  printf("                   no image from this year should be included!\n");
   printf("\n");
 
   exit(exit_code);
@@ -58,11 +64,19 @@ void usage(char *exe, int exit_code){
 }
 
 void parse_args(int argc, char *argv[], args_t *args){
-int opt, received_n = 0, expected_n = 8;
+int opt, received_n = 0, expected_n = 9;
   opterr = 0;
 
-  while ((opt = getopt(argc, argv, "r:c:m:t:e:s:n:y:")) != -1){
+  while ((opt = getopt(argc, argv, "j:x:r:c:m:t:e:s:n:")) != -1){
     switch(opt){
+      case 'j':
+        args->n_cpus = atoi(optarg);
+        received_n++;
+        break;
+      case 'x':
+        copy_string(args->path_mask, STRLEN, optarg);
+        received_n++;
+        break;
       case 'r':
         copy_string(args->path_output_reference_period, STRLEN, optarg);
         received_n++;
@@ -91,10 +105,6 @@ int opt, received_n = 0, expected_n = 8;
         args->confirmation_number = atoi(optarg);
         received_n++;
         break;  
-      case 'y':
-        args->current = atoi(optarg);
-        received_n++;
-        break;
       case '?':
         if (isprint(optopt)){
           fprintf(stderr, "Unknown option `-%c'.\n", optopt);
@@ -137,6 +147,11 @@ int opt, received_n = 0, expected_n = 8;
     usage(argv[0], FAILURE);
   }
 
+  if (args->n_cpus < 1){
+    fprintf(stderr, "Number of CPUs must be at least 1.\n");
+    usage(argv[0], FAILURE);
+  }
+
   if (args->modes != 1 && args->modes != 2 && args->modes != 3){
     fprintf(stderr, "modes must be between 1, 2, or 3.\n");
     usage(argv[0], FAILURE);
@@ -162,11 +177,6 @@ int opt, received_n = 0, expected_n = 8;
     usage(argv[0], FAILURE);
   }
 
-  if (args->current < 1970 || args->current > 2100){
-    fprintf(stderr, "current year must be between 1970 and 2100. Or even better a reasonable year\n");
-    usage(argv[0], FAILURE);
-  }
-
   return;
 }
 
@@ -177,12 +187,16 @@ int main ( int argc, char *argv[] ){
 args_t args;
 date_t *dates = NULL;
 image_t *input = NULL;
+image_t mask;
 image_t reference_period;
 image_t coefficients;
+
 
   parse_args(argc, argv, &args);
 
   GDALAllRegister();
+
+  read_image(args.path_mask, NULL, &mask);
 
   alloc((void**)&input, args.n_images, sizeof(image_t));
   alloc((void**)&dates, args.n_images, sizeof(date_t));
@@ -194,9 +208,9 @@ image_t coefficients;
     date_from_string(&dates[i], basename);
 
     read_image(args.path_input[i], NULL, &input[i]);
+    compare_images(&mask, &input[i]);
 
     if (i > 0){
-      compare_images(&input[0], &input[i]);
       if (dates[i].ce < dates[i-1].ce){
         fprintf(stderr, "Input images must be ordered by date (earliest to latest).\n");
         exit(FAILURE);
@@ -205,39 +219,68 @@ image_t coefficients;
 
   }
 
-
-  int n_coef = 1 + args.modes * 2 + (args.trend ? 1 : 0);
-
-  // at least have offset and uni-modal frequency
-  if (n_coef < 3){
-    printf("not enough coefficients for harmonic fitting. "
-      "Something is wrong. Abort.\n");
-      return FAILURE;
+  int n_periods = dates[args.n_images - 1].year - args.fit_end + 1; // including 1st and next year
+  int **periods = NULL;
+  alloc_2D((void***)&periods, n_periods, 2, sizeof(int));
+  for (int i=1, period=0; i<args.n_images; i++){
+    if (dates[i].year > dates[i-1].year && dates[i].year > args.fit_end){
+      periods[period][0] = dates[i].year-1;
+      periods[period][1] = i;
+      period++;
+    }
   }
+  periods[n_periods - 1][0] = dates[args.n_images - 1].year;
+  periods[n_periods - 1][1] = args.n_images;
+
+  /** Example
+  9 fitting periods in time series
+  Fitting period 0 ends in year 2017 below index 59
+  Fitting period 1 ends in year 2018 below index 119
+  Fitting period 2 ends in year 2019 below index 170
+  Fitting period 3 ends in year 2020 below index 236
+  Fitting period 4 ends in year 2021 below index 280
+  Fitting period 5 ends in year 2022 below index 339
+  Fitting period 6 ends in year 2023 below index 385
+  Fitting period 7 ends in year 2024 below index 419
+  Fitting period 8 ends in year 2025 below index 473
+  **/
+
+  printf("%d fitting periods in time series\n", n_periods);
+  for (int period=0; period<n_periods; period++){
+    printf("Fitting period %d ends in year %d below index %d\n", period, periods[period][0], periods[period][1]);
+  }
+
   
-  gsl_set_error_handler_off();
-  
+  int n_coef = number_of_coefficients(args.modes, args.trend);
   
   copy_image(&input[0], &reference_period, 1, SHRT_MIN, args.path_output_reference_period);
   copy_image(&input[0], &coefficients, n_coef, SHRT_MIN, args.path_output_coefficient);
+  
+  // pre-compute terms for harmonic fitting
+  float **terms;
+  alloc_2D((void***)&terms, args.n_images, n_coef, sizeof(float));
+  compute_harmonic_terms(dates, args.n_images, args.modes, args.trend, terms);
+  
+  gsl_set_error_handler_off();
 
+  omp_set_num_threads(args.n_cpus);
 
-  omp_set_num_threads(64);
-  //omp_set_num_threads(omp_get_thread_limit());
-
-  #pragma omp parallel shared(args, dates, input, reference_period, coefficients, n_coef) default(none)
+  #pragma omp parallel shared(args, dates, periods, n_periods, input, mask, terms, reference_period, coefficients, n_coef) default(none)
   {
 
   gsl_vector *c = gsl_vector_alloc(n_coef);
   gsl_matrix *cov = gsl_matrix_alloc(n_coef, n_coef);
   gsl_vector *x_pred = gsl_vector_alloc(n_coef);
 
+
   #pragma omp for
   for (int p=0; p<reference_period.nc; p++){
-
+//if (p != 503971) continue;
     // initialize images
     for (int b=0; b<coefficients.nb; b++) coefficients.data[b][p] = coefficients.nodata;
     reference_period.data[0][p] = reference_period.nodata;
+
+    if (mask.data[0][p] == mask.nodata || mask.data[0][p] == 0) continue;
 
     bool all_nodata = true;
     for (int i=0; i<args.n_images; i++){
@@ -248,37 +291,22 @@ image_t coefficients;
     }
     if (all_nodata) continue;
 
+
     // reset iterators
-    int local_end = args.fit_end;
-    bool stable = true;
+    int fit_period = 0;
 
-    while (local_end < (args.current - 1) && stable){
+    while (fit_period < n_periods){
 
-      int i_end = 0;
       int n_valid = 0;
 
-      // pre-screen and determine fitting range
-      for (int i=i_end; i<args.n_images; i++){
-
-        if (dates[i].year > local_end){
-          i_end = i;
-          break;
-        } else if (input[i].data[0][p] == input[i].nodata){
-          continue;
-        } else {
-          n_valid++;
-        }
-
-        if (i == args.n_images - 1) i_end = args.n_images;
-
+      // pre-screen number of valid observations in fitting period
+      for (int i=0; i<periods[fit_period][1]; i++){
+        if (input[i].data[0][p] != input[i].nodata) n_valid++;
       }
-
-      //printf("Pixel %d: fitting period until %d (index %d), n_valid = %d\n", p, local_end, i_end, n_valid);
-
-    //printf("n_valid: %d\n", n_valid);
+      //printf("n_valid: %d\n", n_valid);
 
       if (n_valid < n_coef){
-        local_end++;
+        fit_period++;
         continue;
       }
 
@@ -287,34 +315,13 @@ image_t coefficients;
       gsl_vector *y = gsl_vector_alloc(n_valid);
 
 
-      for (int i=0, k=0; i<i_end; i++){
+      for (int i=0, k=0; i<periods[fit_period][1]; i++){
 
         if (input[i].data[0][p] == input[i].nodata) continue;
 
-        int i_coef = 0;
-
-        // offset
-        gsl_matrix_set(x, k, i_coef++, 1.0);
-
-        // trend
-        if (args.trend) gsl_matrix_set(x, k, i_coef++, input[i].data[0][p]);
-
-        // uni-modal frequency
-        if (args.modes >= 1){
-          gsl_matrix_set(x, k, i_coef++, cos(2 * M_PI / 365 * dates[i].ce));
-          gsl_matrix_set(x, k, i_coef++, sin(2 * M_PI / 365 * dates[i].ce));
-        }
-
-        // bi-modal frequency
-        if (args.modes >= 2){
-          gsl_matrix_set(x, k, i_coef++, cos(4 * M_PI / 365 * dates[i].ce));
-          gsl_matrix_set(x, k, i_coef++, sin(4 * M_PI / 365 * dates[i].ce));
-        }
-
-        // tri-modal frequency
-        if (args.modes >= 3){
-          gsl_matrix_set(x, k, i_coef++, cos(6 * M_PI / 365 * dates[i].ce));
-          gsl_matrix_set(x, k, i_coef++, sin(6 * M_PI / 365 * dates[i].ce));
+        // explanatory variables
+        for (int coef=0; coef<n_coef; coef++){
+          gsl_matrix_set(x, k, coef, terms[i][coef]);
         }
 
         // response variable
@@ -326,45 +333,32 @@ image_t coefficients;
       // Iteratively Reweighted Least Squares (IRLS)
       irls_fit(x, y, c, cov);
 
-      for (int e=i_end, dist_ctr=0; e<args.n_images; e++){
+      // reached the end of the time series, no data left to predict and compare
+      if (fit_period == (n_periods-1)) break; 
 
-        // only predict for dates within one year after fitting period
-        if (dates[e].year > (local_end + 1) ) break;
+      int predict_period = fit_period + 1;
+      bool stable = true;
+
+      // predict and check for disturbance in subsequent fitting periods
+      for (int e=periods[fit_period][1], dist_ctr=0; e<args.n_images; e++){
 
         if (input[e].data[0][p] == input[e].nodata) continue;
 
-        int i_coef = 0;
-
-        // offset
-        gsl_vector_set(x_pred, i_coef++, 1.0);
-
-        // trend
-        if (args.trend) gsl_vector_set(x_pred, i_coef++, dates[e].ce);
-
-        // uni-modal frequency
-        if (args.modes >= 1){
-          gsl_vector_set(x_pred, i_coef++, cos(2 * M_PI / 365 * dates[e].ce));
-          gsl_vector_set(x_pred, i_coef++, sin(2 * M_PI / 365 * dates[e].ce));
+        if (e >= periods[predict_period][1]) predict_period++;
+ 
+        // explanatory variables
+        for (int coef=0; coef<n_coef; coef++){
+          gsl_vector_set(x_pred, coef, terms[e][coef]);
         }
 
-        // bi-modal frequency
-        if (args.modes >= 2){
-          gsl_vector_set(x_pred, i_coef++, cos(4 * M_PI / 365 * dates[e].ce));
-          gsl_vector_set(x_pred, i_coef++, sin(4 * M_PI / 365 * dates[e].ce));
-        }
-
-        // tri-modal frequency
-        if (args.modes >= 3){
-          gsl_vector_set(x_pred, i_coef++, cos(6 * M_PI / 365 * dates[e].ce));
-          gsl_vector_set(x_pred, i_coef++, sin(6 * M_PI / 365 * dates[e].ce));
-        }
-
+        // predict value and compute residual
         double y_pred, y_err;
         gsl_multifit_robust_est(x_pred, c, cov, &y_pred, &y_err);
         double residual = input[e].data[0][p] - y_pred;
 
         //printf("  Predicting date %d-%d-%d (index %d): observed = %d, predicted = %.2f, residual = %.2f\n",
         //  dates[e].year, dates[e].month, dates[e].day, e, input[e].data[0][p], y_pred, residual);
+        //printf("  Fitting period %d, predicting period %d\n", fit_period, predict_period);
 
         if (args.threshold > 0 && residual > args.threshold){
           dist_ctr++;
@@ -374,36 +368,45 @@ image_t coefficients;
           dist_ctr = 0;
         }
 
-        //printf("    disturbance counter = %d\n", dist_ctr);
+        //printf("    anomaly counter = %d\n", dist_ctr);
 
         if (dist_ctr >= args.confirmation_number){
-          //printf("    -> detected disturbance. Stopping fitting period extension.\n");
+          //printf("    -> detected anomaly. Stop the fitting period extension.\n");
           stable = false;
           break;
         }
 
       }
 
-      gsl_matrix_free (x);
-      gsl_vector_free (y);
+      gsl_matrix_free(x);
+      gsl_vector_free(y);
 
-      //printf("    stil stable, adding one year to fitting period.\n");
-      local_end++;
+      if (stable){
+        // if still stable, extend fitting period to whole time frame, then fit once more
+        fit_period = n_periods - 1;
+      } else if (predict_period == (fit_period + 1)){
+        // if instability detected in the first subsequent fitting period, stop the fitting process
+        break;
+      } else {
+        // otherwise, fit again up to the fitting period before instability detected
+        fit_period = predict_period - 1;
+      }
 
     }
 
 
     // save coefficients and reference period
-    reference_period.data[0][p] = local_end;
+    reference_period.data[0][p] = periods[fit_period][0]; // last stable year
     for (int b=0; b<n_coef; b++){
-      coefficients.data[b][p] = (short)gsl_vector_get(c, b);
+      //printf("Pixel %d, Coefficient %d: %.2f\n", p, b, gsl_vector_get(c, b));
+      coefficients.data[b][p] = (short)(gsl_vector_get(c, b) * _COEF_SCALE_);
     }
 
   }
 
-  gsl_vector_free (c);
-  gsl_matrix_free (cov);
-  gsl_vector_free (x_pred);
+  gsl_vector_free(c);
+  gsl_matrix_free(cov);
+  gsl_vector_free(x_pred);
   gsl_set_error_handler(NULL);
   
   } // end omp parallel region
@@ -411,11 +414,13 @@ image_t coefficients;
   write_image(&reference_period);
   write_image(&coefficients);
 
-  
+
+  free_2D((void**)terms, args.n_images);
   for (int i=0; i<args.n_images; i++){
     free_image(&input[i]);
   }
   free((void*)input);
+  free_image(&mask);
   free_image(&reference_period);
   free_image(&coefficients);
   free((void*)dates);
