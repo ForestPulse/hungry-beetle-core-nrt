4479d54df15c3aaf619d50fb386ecc959b464098
David Frantz <david.frantz@uni-trier.de>
2025-12-09 15:41:49 +0100

    harmonic function fitting

--- src/utils/harmonic.c
+++ src/utils/harmonic.c
@@ -6,9 +6,92 @@ This file contains functions for fitting harmonic models to time series
 #include "harmonic.h"
 
 
+int number_of_coefficients(int modes, int trend){
+
+  int n_coef = 1 + modes * 2 + (trend ? 1 : 0);
+  
+  // at least have offset and uni-modal frequency
+  if (n_coef < 3){
+    printf("not enough coefficients for harmonic fitting. "
+      "Something is wrong. Abort.\n");
+      return FAILURE;
+  }
+
+  return n_coef;
+}
+
+void compute_harmonic_terms(date_t *dates, int n_dates, int modes, int trend, float **terms){
+
+
+  for (int i=0; i<n_dates; i++){
+
+    int coef = 0;
+
+    terms[i][coef++] = 1.0; // intercept
+
+    if (trend){
+      terms[i][coef++] = (float)(dates[i].ce); // trend
+    }
+
+    // uni-modal frequency
+    if (modes >= 1){
+      terms[i][coef++] = cos(2 * M_PI / 365 * dates[i].ce);
+      terms[i][coef++] = sin(2 * M_PI / 365 * dates[i].ce);
+    }
+
+    // bi-modal frequency
+    if (modes >= 2){
+      terms[i][coef++] = cos(4 * M_PI / 365 * dates[i].ce);
+      terms[i][coef++] = sin(4 * M_PI / 365 * dates[i].ce);
+    }
+
+    // tri-modal frequency
+    if (modes >= 3){
+      terms[i][coef++] = cos(6 * M_PI / 365 * dates[i].ce);
+      terms[i][coef++] = sin(6 * M_PI / 365 * dates[i].ce);
+    }
+
+  }
+
+  return;
+}
+
+
+float predict_harmonic_value(float *x, image_t *coefficients, int pixel, int n_coef, int modes, int trend){
+
+  int coef = 0;
+
+  // offset
+  float y_pred = x[coef++];
+
+  // trend
+  if (trend){
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+  } 
+
+  // uni-modal frequency
+  if (modes >= 1){
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+  }
+
+  // bi-modal frequency
+  if (modes >= 2){
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+  }
+
+  // tri-modal frequency
+  if (modes >= 3){
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+    y_pred += x[coef] * coefficients->data[coef][pixel] / _COEF_SCALE_; coef++;
+  }
+
+  return y_pred;
+}
+
 
 int irls_fit(const gsl_matrix *X, const gsl_vector *y, gsl_vector *c, gsl_matrix *cov){
-
 
   gsl_multifit_robust_workspace *work = 
     gsl_multifit_robust_alloc(gsl_multifit_robust_bisquare, X->size1, X->size2);
--- src/utils/harmonic.h
+++ src/utils/harmonic.h
@@ -12,6 +12,8 @@ Harmonic model header
 #include <stdbool.h>  // boolean data type
 
 #include "const.h"
+#include "date.h"
+#include "image_io.h"
 
 /** GNU Scientific Library (GSL) **/
 #include <gsl/gsl_multifit.h> // Linear Least Squares Fitting
@@ -20,7 +22,11 @@ Harmonic model header
 extern "C" {
 #endif
 
+#define _COEF_SCALE_ 10.0f
 
+int number_of_coefficients(int modes, int trend);
+void compute_harmonic_terms(date_t *dates, int n_dates, int modes, int trend, float **terms);
+float predict_harmonic_value(float *x, image_t *coefficients, int pixel, int n_coef, int modes, int trend);
 int irls_fit(const gsl_matrix *X, const gsl_vector *y, gsl_vector *c, gsl_matrix *cov);
 
 #ifdef __cplusplus
